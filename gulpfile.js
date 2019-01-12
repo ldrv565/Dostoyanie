71dc9ed38377ce1966c3471cbb6eb78b9f983a11
ldrv565 <40745264+ldrv565@users.noreply.github.com>
2019-01-12 08:59:32 +0300

    Промо-блок, карусель, хедер, футер

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp'), // Подключаем Gulp
 	sass = require('gulp-sass'), //Подключаем Sass пакет,
 	browserSync = require('browser-sync'); // Подключаем Browser Sync
-	gcmq = require('gulp-group-css-media-queries');
+gcmq = require('gulp-group-css-media-queries');
 
-gulp.task('sass', function() { // Создаем таск Sass
+gulp.task('sass', function () { // Создаем таск Sass
 	return gulp.src('app/sass/*.sass') // Берем источник
 		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
 		.pipe(gcmq())
@@ -13,7 +13,7 @@ gulp.task('sass', function() { // Создаем таск Sass
 		})) // Обновляем CSS на странице при изменении
 });
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
+gulp.task('browser-sync', function () { // Создаем таск browser-sync
 	browserSync({ // Выполняем browserSync
 		server: { // Определяем параметры сервера
 			baseDir: 'app' // Директория для сервера - app
@@ -22,10 +22,14 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 	});
 });
 
-gulp.task('watch', gulp.parallel('browser-sync', 'sass',function() {
+gulp.task('watch', gulp.parallel('browser-sync', 'sass', function () {
 	gulp.watch('app/sass/*.sass').on('change', gulp.series('sass')); // Наблюдение за sass файлами в папке sass
-	gulp.watch('app/*.html').on("change", function(){browserSync.reload}); // Наблюдение за HTML файлами в корне проекта
-	gulp.watch('app/js/*.js').on("change", function(){browserSync.reload});  // Наблюдение за JS файлами в папке js
+	gulp.watch('app/*.html').on("change", function () {
+		browserSync.reload()
+	}); // Наблюдение за HTML файлами в корне проекта
+	gulp.watch('app/js/*.js').on("change", function () {
+		browserSync.reload()
+	}); // Наблюдение за JS файлами в папке js
 }));
 
 gulp.task('default', gulp.series('watch'));
